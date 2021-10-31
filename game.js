d960ed905e70b03a8ef00f03af6cbed04b240c9c
BenCoke12 <91262710+BenCoke12@users.noreply.github.com>
2021-10-31 19:54:42 +0000

    lot of work

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 function Bear() {
-  this.dBear = 100;
+  this.dBear = 10;
   this.htmlElement = document.getElementById("bear");
   this.id = this.htmlElement.id;
   this.x = this.htmlElement.offsetLeft;
@@ -7,10 +7,10 @@ function Bear() {
 
   this.move = function (xDir, yDir) {
     this.fitBounds(); //add this instruction to keep bear within the board
+    this.setSpeed(); //
     this.x += this.dBear * xDir;
     this.y += this.dBear * yDir;
     this.display();
-    console.log(this.dBear);
   };
 
   this.display = function () {
@@ -24,8 +24,8 @@ function Bear() {
     let parent = this.htmlElement.parentElement;
     let iw = this.htmlElement.offsetWidth;
     let ih = this.htmlElement.offsetHeight;
-    let l = parent.offsetLeft;
-    let t = parent.offsetTop;
+    let l = parent.offsetLeft; //is this used anywhere?
+    let t = parent.offsetTop; //also
     let w = parent.offsetWidth;
     let h = parent.offsetHeight;
     if (this.x < 0) this.x = 0;
@@ -34,9 +34,8 @@ function Bear() {
     if (this.y > h - ih) this.y = h - ih;
   };
 
-  setSpeed = function () {
-    this.dBear = document.getElementById("speedBear").value;
-    console.log(this.dBear);
+  this.setSpeed = function () {
+    this.dBear = parseInt(document.getElementById("speedBear").value);
   };
 }
 
@@ -45,7 +44,14 @@ function start() {
   bear = new Bear();
   //add an event listener to the keypress event
   document.addEventListener("keydown", moveBear, false);
-  document.addEventListener("change", setSpeed, false);
+  //not needed when setSpeed is called inside this.move
+  //document.addEventListener("change", bear.setSpeed, false);
+  //create new array for bees
+  bees = new Array();
+  //create bees
+  makeBees();
+  //initial call for updateBees
+  updateBees();
 }
 
 //Handle keyboard events
@@ -69,4 +75,118 @@ function moveBear(e) {
   if (e.keyCode == KEYDOWN) {
     bear.move(0, 1);
   } //down key
+}
+
+class Bee {
+  constructor(beeNumber) {
+    //the HTML element corresponding to the IMG of the bee
+    this.htmlElement = createBeeImg(beeNumber);
+    //its HTML ID
+    this.id = this.htmlElement.id;
+    //the left position (x)
+    this.x = this.htmlElement.offsetLeft;
+    //the top position (y)
+    this.y = this.htmlElement.offsetTop;
+
+    this.move = function (dx, dy) {
+      //move the bees by dx, dy
+      this.x += dx;
+      this.y += dy;
+      this.display();
+    };
+
+    this.display = function () {
+      //adjust position of bee and display it
+      this.fitBounds(); //add to adjust to bounds
+      this.htmlElement.style.left = this.x + "px";
+      this.htmlElement.style.top = this.y + "px";
+      this.htmlElement.style.display = "block";
+    };
+
+    this.fitBounds = function () {
+      //check and make sure the bees stay in the board space
+      let parent = this.htmlElement.parentElement;
+      let iw = this.htmlElement.offsetWidth;
+      let ih = this.htmlElement.offsetHeight;
+      let l = parent.offsetLeft; //also
+      let t = parent.offsetTop; //also
+      let w = parent.offsetWidth;
+      let h = parent.offsetHeight;
+      if (this.x < 0) this.x = 0;
+      if (this.x > w - iw) this.x = w - iw;
+      if (this.y < 0) this.y = 0;
+      if (this.y > h - ih) this.y = h - ih;
+    };
+  }
+}
+
+function createBeeImg(wNum) {
+  //getdimension and position of board div
+  let boardDiv = document.getElementById("board");
+  let boardDivW = boardDiv.offsetWidth;
+  let boardDivH = boardDiv.offsetHeight;
+  let boardDivX = boardDiv.offsetLeft;
+  let boardDivY = boardDiv.offsetTop;
+  //create the IMG element
+  let img = document.createElement("img");
+  img.setAttribute("src", "images/bee.gif");
+  img.setAttribute("width", "100");
+  img.setAttribute("alt", "A bee!");
+  img.setAttribute("id", "bee" + wNum);
+  img.setAttribute("class", "bee"); //set class of html tag img
+  //add the IMG element to the DOM as a child of the board div
+  img.style.position = "absolute";
+  boardDiv.appendChild(img);
+  //set initial position
+  let x = getRandomInt(boardDivW);
+  let y = getRandomInt(boardDivH);
+  img.style.left = boardDivX + x + "px";
+  img.style.top = y + "px";
+  //return the img object
+  return img;
+}
+
+function getRandomInt(max) {
+  return Math.random() * max;
+}
+
+function makeBees() {
+  //get number of bees specified by the use
+  let nbBees = document.getElementById("nbBees").value;
+  nbBees = Number(nbBees); //try converting the content of the input to a number
+  if (isNaN(nbBees)) {
+    //check that the input field contains a valid number
+    window.alert("Invalid number of bees");
+    return;
+  }
+  //create bees
+  let i = 1;
+  while (i <= nbBees) {
+    var num = i;
+    var bee = new Bee(num); //create object and its IMG element
+    bee.display(); //display the bee
+    bees.push(bee); //add the bee object to the bees array
+    i++;
+  }
+}
+
+function moveBees() {
+  //get speed input field value
+  let speed = document.getElementById("speedBees").value;
+  //move each bee to a random location
+  for (let i = 0; i < bees.length; i++) {
+    let dx = getRandomInt(2 * speed) - speed;
+    let dy = getRandomInt(2 * speed) - speed;
+    bees[i].move(dx, dy);
+  }
+}
+
+function updateBees() {
+  //update loop for game
+  //move the bees randomly
+  moveBees();
+  //use a fixed update period
+  let period = 10; //modify to control refesh period
+  //update teh timer for the next move
+  updateTimer = setTimeout("updateBees()", period);
 }
